7280676b94d19e95d47b86b6fab390d5a7865478
Pan Shaun <Shaun.Pan@fil.com>
2023-04-21 11:47:44 +0800

    add tokyonight transparent again

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -303,4 +303,17 @@ return {
     end,
   },
   { "ThePrimeagen/harpoon", event = "VeryLazy" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      require("notify").setup({
+        background_colour = "#000000",
+      }),
+    },
+  },
 }
