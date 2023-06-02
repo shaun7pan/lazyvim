c0bb537a09602a63c49ebb0ea25a66db0ce9bf0b
Pan Shaun <Shaun.Pan@fil.com>
2023-06-02 16:04:45 +0800

    Remove vim-tmux-navigator

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,11 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 
 return {
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-
   -- add symbols-outline
   {
     "simrat39/symbols-outline.nvim",
