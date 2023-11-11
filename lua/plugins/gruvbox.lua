38fece816057eab62082b7f6b15cf0c83da0cd3b
Pan Shaun <Shaun.Pan@fil.com>
2023-11-11 11:51:07 +0800

    Make octo.nvim to be lazy loaded via cmd starging with `Octo`

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,5 +1,5 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
   -- Configure LazyVim to load gruvbox
   -- {
   --   "LazyVim/LazyVim",
--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -3,9 +3,14 @@
 return {
   {
     "pwntester/octo.nvim",
-    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
     config = function()
       require("octo").setup()
     end,
+    cmd = "Octo",
   },
 }
