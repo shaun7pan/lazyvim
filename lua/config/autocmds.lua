64977b11cc759afa3ca95bbe3240920b810dd562
Pan Shaun <Shaun.Pan@fil.com>
2023-11-16 22:35:04 +0800

    Fix notify issue

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,4 +2,4 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- vim.cmd([[ hi NotifyBackground guibg = #000000 ]])
+vim.cmd([[ hi NotifyBackground guibg = #000000 ]])
