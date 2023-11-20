877dbc15fdb3e5f266bcbcfdccc2888692e414d8
Pan Shaun <Shaun.Pan@fil.com>
2023-11-20 10:34:18 +0800

    Add multi cursor alternative

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,11 @@
 -- Add any additional autocmds here
 
 vim.cmd([[ hi NotifyBackground guibg = #000000 ]])
+
+-- multi cursor alternative (visual highlight first, better than * cgn)
+
+-- change the word, press `n` searching for the next, press `.` repeat.
+vim.cmd([[nnoremap <leader>cw <cmd>let @/='\<'.expand('<cword>').'\>'<cr>"_ciw]])
+
+-- virtual mode, select the change
+vim.cmd([[xnoremap <leader>cw y<cmd>let @/=substitute(escape(@", '/'), '\n', '\\n', 'g')<cr>"_cgn]])
