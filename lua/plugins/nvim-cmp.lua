3ed270a73c9aa93f5b9c9aca8e650beac9481be1
Pan Shaun <Shaun.Pan@fil.com>
2024-03-07 18:42:53 +0800

    **Change to use Tab to confirm selection, CR is still working

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -28,7 +28,9 @@ return {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
+            -- cmp.select_next_item()
+            cmp.confirm({ select = true })
+
           -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
           -- this way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
