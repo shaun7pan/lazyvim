d150d4e22678d869b6af7812d5460ada1531d7fc
Pan Shaun <Shaun.Pan@fil.com>
2024-03-09 10:23:57 +0800

    - Add .gitignore ignoring lazy-lock.json - Remove shift-enter key settings in supper-tab, leave ctrl-enter   settings only. This is because after set "[13;2u" and "[13;5u" the key   combination had been working in tmux, so the default key mappings in   nvim-cmp will taking effect. But I don't want ctrl-enter to be ignored   first and insert new line later that's why I leave the override   settings here.

--- lua/plugins/supper-tab.lua
+++ lua/plugins/supper-tab.lua
@@ -37,12 +37,6 @@ return {
       })
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<S-CR>"] = cmp.mapping(function(fallback)
-          fallback()
-        end, { "i", "s" }),
-      })
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
