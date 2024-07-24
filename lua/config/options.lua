bc3eb22ca7fbfa64cea76e91a7be428d94e56d07
Pan Shaun <Shaun.Pan@fil.com>
2024-07-24 13:10:37 +0800

    After switch to jamf cloud

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,7 @@ local opt = vim.opt
 opt.relativenumber = false -- Relative line numbers
 opt.colorcolumn = "100"
 opt.mouse = ""
+
+vim.g.python3_host_prog = "/Users/shaun.pan@fil.com/.pyenv/versions/3.12.0/bin/python"
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
